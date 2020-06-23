42ac0a09bc708f9e487d7ed242959f4b9f828049
khaled-codeur <manou-codeur@outlook.com>
2020-06-23 17:57:23 +0100

    start the app

---
+++ src/components/RemoveBtn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RemoveBtn = props => {
+  return <h1>this is the remove btn</h1>;
+};
+
+export default RemoveBtn;
---
+++ src/components/SelectedBtn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectedBtn = props => {
+  return <h1>this is the selected btn</h1>;
+};
+
+export default SelectedBtn;
